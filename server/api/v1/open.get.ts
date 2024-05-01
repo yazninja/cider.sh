3287259d658be2f49e781a6c494d0434575b7ad3
yazninja <yazninja@cider.sh>
2024-05-01 13:09:07 +0300

    add v1 (legacy) deeplinks

--- server/api/v1/open.get.ts
+++ server/api/v1/open.get.ts
@@ -1,5 +1,8 @@
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
   const query = getQuery(event);
 
-  return { query: query };
+  if (query.id && query.s) {
+    return await sendRedirect(event, `cider://open/s/${query.id}`);
+  }
+  return await sendRedirect(event, `cider://${event.path.split("?")[1]}`);
 });
